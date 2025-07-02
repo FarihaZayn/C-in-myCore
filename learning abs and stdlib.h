e28333d4aaa6d38abe7c7ea7c407cf38f6ff0585
Fariha Tahsin Proma <zfarihatahsinproma@gmail.com>
2025-07-03 05:08:54 +0600

    Add files via upload

---
+++ learning abs and stdlib.h
@@ -0,0 +1,21 @@
+
+//this wud be my first time workijng with stdlib.h
+//bascically eda holo janina
+//now lets talk about abs which means an absolute number
+//which removes signs ,if it is positive then it is positive; if it is neg then it becomes +ve.
+
+/*Make a program that reads 3 integer values and present the greatest one followed by the message "eh o maior". Use the following formula:
+
+/* Online C compiler to run C program online
+*/
+#include <stdio.h>
+#include<stdlib.h>
+
+int main() {
+    // Write C code here
+    int a=-906,b=abs(a);
+    printf("%i",b);
+
+    return 0;
+}
+
