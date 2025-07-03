1fdf3e866152baf7011d616724f34be86c830b14
Fariha Tahsin Proma <zfarihatahsinproma@gmail.com>
2025-07-03 14:11:43 +0600

    Add files via upload

---
+++ bee 1016.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%i",&a);
+    printf("%i minutos\n",a*2);
+
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+
+    return 0;
+}
