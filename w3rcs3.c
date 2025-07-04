b448efcad023859978b60ede9af5006561847dcc
Fariha Tahsin Proma <zfarihatahsinproma@gmail.com>
2025-07-04 23:15:56 +0600

    Add files via upload

---
+++ w3rcs3.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){
+    int a;
+    printf("Test Data: ");
+    scanf("%d",&a);
+    if (a>0){
+        printf("%i is positive number",a);
+
+    }
+    else {
+        printf("neg");
+    }
+
+}
+
+
+/*3. Positive or Negative Check
+
+Write a C program to check whether a given number is positive or negative.
+Test Data : 15
+Expected Output :
+15 is a positive number
+
+Click me to see the solution
+
+
+*/
