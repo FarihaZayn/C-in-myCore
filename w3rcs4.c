0fe694d2921a5acb1cf8777469a62c8ba9a974c5
Fariha Tahsin Proma <zfarihatahsinproma@gmail.com>
2025-07-04 23:39:33 +0600

    Add files via upload

---
+++ w3rcs4.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+    int a;
+    printf("Test Data: ");
+    scanf("%d",&a);
+    if (18<=a){
+        printf("Congratulation! You are eligible for casting your vote.");
+
+    }
+    else {
+        printf("u cant1");
+    }
+
+}
+
+
+/*/5. Voting Eligibility
+
+Write a C program to read the age of a candidate and determine whether he is eligible to cast his/her own vote.
+Test Data : 21
+Expected Output :
+Congratulation! You are eligible for casting your vote.
+
+Click me to see the solution*/
